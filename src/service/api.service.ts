743ee42841074ae5d21a41032a0191ed4a020cb6
WlncEstEr <WlncEstEr@email.com>
2026-07-14 15:42:36 +0300

    Add .env and add const in 'api.service.ts'

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -4,12 +4,14 @@ import type { IApiResponse } from '../types/api.types'
 class ApiService {
 	public URL = 'https://api.poiskkino.dev/v1.4/movie'
 
+	apiKey = import.meta.env.VITE_API_KEY || ''
+
 	async getApiById(endpoint: number) {
 		const response = await axios.get<IApiResponse | null>(
 			`${this.URL}/${endpoint}`,
 			{
 				headers: {
-					'X-API-KEY': 'ED1N4EH-3P14Y6B-JPPQMR6-50MCFZ4',
+					'X-API-KEY': this.apiKey,
 					'Content-Type': 'application/json'
 				}
 			}
